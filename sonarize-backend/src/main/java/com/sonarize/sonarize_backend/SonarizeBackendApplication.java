a918508ab3e21077b997cdef54eb42b7d7a806ed
Batman <wgruba08@gmail.com>
2024-11-03 14:53:55 +0100

    Initialize backend application

---
+++ sonarize-backend/src/main/java/com/sonarize/sonarize_backend/SonarizeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sonarize.sonarize_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarizeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarizeBackendApplication.class, args);
+	}
+
+}
---
+++ sonarize-backend/src/test/java/com/sonarize/sonarize_backend/SonarizeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonarize.sonarize_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarizeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

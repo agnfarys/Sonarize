a6dd71081556426fc92743f45bf4bf78254cc98b
Batman <wgruba08@gmail.com>
2024-11-04 17:00:25 +0100

    add security config to permit all requests

---
+++ sonarize-backend/src/main/java/com/sonarize/sonarize_backend/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.sonarize.sonarize_backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}

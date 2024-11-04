4cc9719b851644804fa6bd6d0d9ff9ff1a737217
Batman <wgruba08@gmail.com>
2024-11-04 16:59:16 +0100

    add data models

---
+++ sonarize-backend/src/main/java/com/sonarize/sonarize_backend/model/Playlist.java
@@ -0,0 +1,20 @@
+package com.sonarize.sonarize_backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document(collection = "playlists")
+@Getter
+@Setter
+public class Playlist {
+    @Id
+    private String id;
+    private String userId;  // Klucz obcy do `users`
+    private List<String> trackUris;
+    private String playlistLink;
+    private String createdAt;
+
+}
---
+++ sonarize-backend/src/main/java/com/sonarize/sonarize_backend/model/Survey.java
@@ -0,0 +1,26 @@
+package com.sonarize.sonarize_backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document(collection = "surveys")
+@Setter
+@Getter
+public class Survey {
+    @Id
+    private String id;
+    private String userId;  // Klucz obcy do `users`
+    private String mood;
+    private List<String> genres;
+    private String energyLevel;
+    private String occasion;
+    private List<String> favoriteArtists;
+    private String discoveryPreference;
+    private String languagePreference;
+    private String playlistLength;
+    private String createdAt;
+
+}

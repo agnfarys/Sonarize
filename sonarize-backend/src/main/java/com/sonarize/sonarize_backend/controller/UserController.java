a80d706dc4aca81d1c6f6f0518e457d1765dac66
Jakub Roszkowski <jakubr1701@gmail.com>
2024-11-05 10:35:57 +0100

    chat integrated

--- sonarize-backend/src/main/java/com/sonarize/sonarize_backend/controller/UserController.java
+++ sonarize-backend/src/main/java/com/sonarize/sonarize_backend/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.sonarize.sonarize_backend.controller;
 
+import com.sonarize.sonarize_backend.model.Survey;
 import com.sonarize.sonarize_backend.model.User;
+import com.sonarize.sonarize_backend.service.ChatGPTService;
 import com.sonarize.sonarize_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,4 +28,25 @@ public class UserController {
     public User findUserByUsername(@RequestParam String username) {
         return userService.findUserByUsername(username);
     }
+
+    @Autowired
+    private ChatGPTService chatGPTService;
+
+    @GetMapping("/test")
+    public List<String> test() {
+        Survey survey = new Survey();
+        survey.setMood("HAPPY");
+        survey.setGenres(List.of("ROCK", "POP"));
+        survey.setEnergyLevel("80");
+        survey.setOccasion("PARTY");
+        survey.setFavoriteArtists(List.of("Roling Stones", "AC/DC"));
+        survey.setDiscoveryPreference("NEW_RELEASES");
+        survey.setLanguagePreference("EN");
+        survey.setPlaylistLength("5");
+
+        List<String> recommendations = chatGPTService.generateRecommendations(survey);
+        System.out.println("Recommended Playlist: " + recommendations);
+        return recommendations;
+    }
+
 }
